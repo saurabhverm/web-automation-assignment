562689a560accb024c81cbf41fecd14cd2ed676e
Chauhan Mayank <saurabhverma@Saurabhs-MacBook-Air.local>
2020-01-26 13:35:09 +0530

    test jenkins pipeline

---
+++ src/test/java/amazonTest/addToCartTest.java
@@ -0,0 +1,12 @@
+package amazonTest;
+
+import org.testng.annotations.Test;
+import testbase.TestBase;
+
+public class addToCartTest extends TestBase {
+
+    @Test
+    public void userShouldAbleToAddPhoneToCartOnAmazon(){
+        System.out.println("Hello World");
+    }
+}
